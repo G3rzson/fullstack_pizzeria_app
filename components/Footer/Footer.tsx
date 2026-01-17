0d28fc878464006820c01c2efae53a7d4bfa8dae
G3rzson <gerzonpal@gmail.com>
2026-01-17 17:06:19 +0100

    added footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-4 text-center text-sm border-t border-zinc-500 bg-zinc-200 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-200">
+      <p>&copy; 2026 Pizzeria App. All rights reserved.</p>
+    </footer>
+  );
+}
