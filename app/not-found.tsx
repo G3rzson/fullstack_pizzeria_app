3cdc5188acfa06fbc4bc583d99a0413be63208d6
G3rzson <gerzonpal@gmail.com>
2026-03-14 10:01:44 +0100

    mod(not-found): changed for custom link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,14 @@
+import CustomLink from "@/components/ui/CustomLink";
 import { ArrowBigLeft } from "lucide-react";
-import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col grow items-center justify-center gap-8">
       <h1 className="page-title">Page Not Found!</h1>
-      <Link
-        className="flex items-center gap-4 text-link hover:text-link/80 transition-colors duration-300 hover:bg-link/10 rounded-md px-4 py-2"
-        href={"/"}
-      >
+
+      <CustomLink href="/">
         <ArrowBigLeft /> Go to the home page
-      </Link>
+      </CustomLink>
     </div>
   );
 }
