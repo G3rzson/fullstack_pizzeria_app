91107aa3d7d42b7964ca106db69ea34b81c45ad1
G3rzson <gerzonpal@gmail.com>
2026-03-14 19:40:40 +0100

    chore: set up Prisma and server action upload limits

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // bodySizeLimit is required to handle file uploads in server actions
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
