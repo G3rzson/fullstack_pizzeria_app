5d1a1b32ae56950aef9ad1ad096fed458034edbb
G3rzson <gerzonpal@gmail.com>
2026-03-14 09:31:04 +0100

    mod(not-found): added page title class

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col grow items-center justify-center gap-8">
-      <h1 className="text-4xl font-bold">Page Not Found!</h1>
+      <h1 className="page-title">Page Not Found!</h1>
       <Link
         className="flex items-center gap-4 text-link hover:text-link/80 transition-colors duration-300 hover:bg-link/10 rounded-md px-4 py-2"
         href={"/"}
