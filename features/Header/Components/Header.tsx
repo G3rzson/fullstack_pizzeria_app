41958e396bb08fed1e0d8e443fa4223ffbd6adc2
G3rzson <gerzonpal@gmail.com>
2026-03-20 17:14:31 +0100

    mod: optimized auth flow

--- features/Header/Components/Header.tsx
+++ features/Header/Components/Header.tsx
@@ -1,30 +1,24 @@
+"use client";
 import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
 import Logo from "./Logo";
 import Link from "next/link";
 import UserMenu from "@/features/User/Components/UserMenu";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-export default async function Header() {
-  const { getPermissions } = getKindeServerSession();
+export default function Header() {
+  const { permissions } = useKindeBrowserClient();
 
-  const show = await getPermissions();
-
-  const isAdmin = show?.permissions.includes("admin-user");
-
-  console.log(show);
-  console.log(isAdmin);
+  const isAdmin = permissions?.permissions.includes("admin-user");
 
   return (
     <header className="w-full border-b bg-background sticky top-0 left-0 z-50">
       <div className="md:w-4/5 w-full mx-auto flex items-center justify-between px-4 py-3">
         <Logo />
 
-        {isAdmin && (
-          <Link href="/admin" className="text-h4">
-            Admin
-          </Link>
-        )}
+        <Link href="/admin" className="text-h4">
+          Admin
+        </Link>
 
         <DesktopNav />
 
