4a9ab87739bc3a09431c6bddd5d0f9ad6f203322
G3rzson <gerzonpal@gmail.com>
2026-04-21 15:22:22 +0200

    fix: fixed prisma client

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,19 +1,19 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+// --- SINGLETON FIX FOR TURBOPACK/NEXT.JS ---
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-const prisma =
-  globalForPrisma.prisma ||
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma;
